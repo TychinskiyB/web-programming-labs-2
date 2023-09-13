033dea384cf3aac567258df3196c5b97f7cd21d1
Богдан Тычинский <tychinskiybogdan@gmail.com>
2023-09-14 00:11:28 +0700

    Меню

--- app.py
+++ app.py
@@ -2,5 +2,54 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
-    return "web-сервер на flask"
+    return """
+
+
+<!doctype html>
+<html>
+    <head>
+        <title>Тычинский Богдан Владимирович, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+        <footer>
+            &copy; Богдан Тычинский, ФБИ-12, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
+@app.route("/lab1")
+def lab1():
+
+    return """
+
+
+<!doctype html>
+<html>
+    <head>
+        <title>Тычинский Богдан Владимирович, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+Flask — фреймворк для создания веб-приложений на языке
+программирования Python, использующий набор инструментов
+Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+называемых микрофреймворков — минималистичных каркасов
+веб-приложений, сознательно предоставляющих лишь самые ба-
+зовые возможности.
+        <h1>web-сервер на flask</h1>
+        <footer>
+            &copy; Богдан Тычинский, ФБИ-12, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
+
