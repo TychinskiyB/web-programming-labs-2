41063d526bf38709d5efbb3e95ff7a984ae2d65f
Богдан Тычинский <tychinskiybogdan@gmail.com>
2023-10-08 21:26:29 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -140,4 +140,19 @@ def qwer():
 '''
 @app.route ('/lab2/example')
 def example():
-    return render_template('example.html')
+    name, lab_num, group, course = 'Тычинский Богдан', 2, 'ФБИ-12', 3
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    return render_template('example.html',
+                            name=name, lab_num=lab_num, group=group,course=course, fruits=fruits )
+
+
+@app.route ('/lab2/')
+def lab2():
+    return render_template('lab2.html')
+    
