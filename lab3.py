e14ee5b85ba9f89a9545bfe70e11517ccc38cf3c
Богдан Тычинский <tychinskiybogdan@gmail.com>
2023-10-27 17:26:09 +0700

    Кларе на проверку

--- lab3.py
+++ lab3.py
@@ -19,3 +19,71 @@ def form1():
         errors['age'] = 'Заполните поле!'
     sex = request.args.get('sex')
     return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+
+    if drink == 'cofee':
+        price == 120 
+    elif drink == 'black-tea':
+        price == 80
+    else:
+        price = 70
+
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template('pay.html', price = price)
+
+@lab3.route('/lab3/succces')
+def succces():
+    return render_template('succes.html')
+
+@lab3.route('/lab3/zach')
+def zach():
+    
+    A = request.args.get("A")
+    B = request.args.get("B")
+    C = request.args.get("C")
+    D = request.args.get("D")
+
+    if A and B and C and D:
+            
+        A = float(A)
+        B = float(B)
+        C = float(C)
+        D = float(D)
+    if A==B==C:
+        result = 4
+    elif A==B==D:
+        result = 3
+    elif A==C==D:
+        result = 2
+    else:
+        result = 1
+
+
+    return render_template('zachita1 laba.html', result = result, A = A, B = B, C = C, D = D)
+
+
+
+
+@lab3.route('/lab3/zac3')
+def zac3():
+    n=6
+    k=2
+    sum=0
+    for i in range(1,n+1):
+        sum=sum+i**k
+    return f"sum={sum}"
+
+
